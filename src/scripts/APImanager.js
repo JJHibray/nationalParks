213d6bb2712540af6324c890fa6b373b61a4ddbf
Josh Hibray <jjhibray@gmail.com>
2019-03-19 00:54:55 -0500

    redid code to display parks

--- src/scripts/APImanager.js
+++ src/scripts/APImanager.js
@@ -1,12 +1,17 @@
-const getParks = () => {
-    return fetch("http://localhost:9099/parks")
-    .then(response => response.json())
+console.log("api")
+
+// const getParks = () => {
+//     return 
+fetch("http://localhost:9099/parks")
+    .then(r => r.json())
     .then(parsedParks => {
         parsedParks.forEach(parks => {
-            const parksAsHTML = parkList(parks)
-            addParksToDom(parksAsHTML)
-
+        const parksAsHTML = buildParkHTML(parks)
+        addParksToDom(parksAsHTML)
         })
-
     })
-}
+
+
+
+// getParks()
+
